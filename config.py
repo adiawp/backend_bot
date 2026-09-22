8e4faaf19f6dad2fe486bc4ca947011d09c1f9bb
adiawp <ngopideh@gmail.com>
2026-09-22 21:10:12 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Masukkan API Key Bybit Anda di sini
-BYBIT_API_KEY = os.getenv("BYBIT_API_KEY", "BeYpTRzxfDYljm4BSo")
-BYBIT_API_SECRET = os.getenv("BYBIT_API_SECRET", "MASUKKAN_SECRET_KEY_ANDA_DISINI")
-
-# Gunakan True untuk Testnet (Demo Trading), False untuk Live
-TESTNET = True
